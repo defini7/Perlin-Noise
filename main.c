fc1df6cbfe720326ea34ca3997a8db0c99c94413
Лучший <75216201+defini7@users.noreply.github.com>
2022-03-19 16:06:06 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,234 @@
+#include <windows.h>
+#include <gl/gl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
+void DisableOpenGL(HWND, HDC, HGLRC);
+
+void PerlinNoise1D(int nCount, float* fSeed, int nOctaves, float* fOutput)
+{
+    for (int x = 0; x < nCount; x++)
+    {
+        float fNoise = 0.0f;
+        float fScale = 1.0f;
+        float fScaleAccumulator = 0.0f;
+
+        for (int o = 0; o < nOctaves; o++)
+        {
+            int nPitch = nCount >> o;
+            int nSample1 = (x / nPitch) * nPitch;
+            int nSample2 = (nSample1 + nPitch) % nCount;
+
+            float fBlend = (float)(x - nSample1) / (float)nPitch;
+            float fSample = (1.0f - fBlend) * fSeed[nSample1] + fBlend * fSeed[nSample2];
+
+            fNoise += fSample * fScale;
+            fScaleAccumulator += fScale;
+            fScale /= 2.0f;
+        }
+
+        fOutput[x] = fNoise / fScaleAccumulator;
+    }
+
+}
+
+int nScreenWidth = 1024;
+int nScreenHeight = 768;
+
+#define OUTPUT_SIZE 1024
+
+float fNoiseSeed1D[OUTPUT_SIZE];
+float fPerlinNoise1D[OUTPUT_SIZE];
+
+int nOctaveCount = 0;
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
+{
+    srand(time(NULL));
+
+    WNDCLASSEX wcex;
+    HWND hwnd;
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
+    BOOL bQuit = FALSE;
+
+    /* register window class */
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_OWNDC;
+    wcex.lpfnWndProc = WindowProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = "GLSample";
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);;
+
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    /* create main window */
+    hwnd = CreateWindowEx(0,
+                          "GLSample",
+                          "Perlin Noise",
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          nScreenWidth,
+                          nScreenHeight,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL(hwnd, &hDC, &hRC);
+
+    glOrtho(0, nScreenWidth, 0, nScreenHeight, 0, 1);
+
+    for (int i = 0; i < OUTPUT_SIZE; i++)
+        fNoiseSeed1D[i] = (float)rand() / (float)RAND_MAX;
+
+    /* program main loop */
+    while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glPushMatrix();
+
+            if (nOctaveCount == 9)
+                nOctaveCount = 1;
+
+            PerlinNoise1D(OUTPUT_SIZE, fNoiseSeed1D, nOctaveCount, fPerlinNoise1D);
+
+            glBegin(GL_LINES);
+            for (int x = 0; x < OUTPUT_SIZE; x++)
+            {
+                int y = fPerlinNoise1D[x] * (float)nScreenHeight / 2.0f + (float)nScreenHeight / 2.0f;
+
+                glColor3f(0.0f, 1.0f, 0.0f);
+                glVertex2f(x, y);
+
+                glColor3f(0.0f, 1.0f, 0.0f);
+                glVertex2f(x, nScreenHeight / 2);
+            }
+            glEnd();
+
+            glPopMatrix();
+
+            SwapBuffers(hDC);
+
+            Sleep (1);
+        }
+    }
+
+    /* shutdown OpenGL */
+    DisableOpenGL(hwnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow(hwnd);
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+        break;
+
+        case WM_DESTROY:
+            return 0;
+
+        case WM_KEYDOWN:
+        {
+            switch (wParam)
+            {
+                case VK_ESCAPE:
+                    PostQuitMessage(0);
+                    break;
+
+                case VK_SPACE:
+                    nOctaveCount++;
+                    break;
+            }
+        }
+        break;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC(hwnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW |
+                  PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+    SetPixelFormat(*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext(*hDC);
+
+    wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(hRC);
+    ReleaseDC(hwnd, hDC);
+}
+
